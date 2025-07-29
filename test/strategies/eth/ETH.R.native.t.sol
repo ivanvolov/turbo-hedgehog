1c2e4bf94c5f6e9fc021ba34ab80313fef1c352b
ivan <vivan.volovik@gmail.com>
2025-07-29 11:52:52 +0200

    add native alm test

--- test/strategies/eth/ETH.R.native.t.sol
+++ test/strategies/eth/ETH.R.native.t.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "forge-std/console.sol";
 
+import "forge-std/console.sol";
+
 // ** contracts
 import {ALMTestBase} from "@test/core/ALMTestBase.sol";
 
@@ -15,7 +17,7 @@ import {IPositionManagerStandard} from "@src/interfaces/IPositionManager.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // This test illustrates the pool with the reversed order of currencies. The main asset first and the stable next.
-contract ETHRALMTest is ALMTestBase {
+contract ETHR_NATIVE_ALMTest is ALMTestBase {
     uint256 longLeverage = 3e18;
     uint256 shortLeverage = 2e18;
     uint256 weight = 55e16; //50%
