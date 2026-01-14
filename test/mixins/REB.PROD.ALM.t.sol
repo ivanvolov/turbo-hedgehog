1107ee47c4afb38400ce0fd2f5020eb2323cb66f
Yevhen <sigmaw39@gmail.com>
2026-01-14 21:43:23 +0100

    rebalance check

--- test/mixins/REB.PROD.ALM.t.sol
+++ test/mixins/REB.PROD.ALM.t.sol
@@ -29,7 +29,7 @@ contract REB_PROD_ALMTest is ALMTestBaseUnichain {
     IERC20 USDC = IERC20(UConstants.USDC);
 
     function setUp() public {
-        select_unichain_fork(32849947 - 1);
+        select_unichain_fork(32845331);
 
         // ** Setting up test environments params
         {
@@ -61,8 +61,66 @@ contract REB_PROD_ALMTest is ALMTestBaseUnichain {
 
     function test_inspect_rebalance() public {
         vm.startPrank(rebalanceAdapter.owner());
+        test_inspect_state();
         rebalanceAdapter.rebalance(50000000000000000);
         vm.stopPrank();
+    }
+
+    function test_inspect_state() public view {
+        console.log("=== SRebalanceAdapter Parameters ===");
+        console.log("Weight:", rebalanceAdapter.weight());
+        console.log("Long Leverage:", rebalanceAdapter.longLeverage());
+        console.log("Short Leverage:", rebalanceAdapter.shortLeverage());
+        console.log("Price Threshold:", rebalanceAdapter.rebalancePriceThreshold());
+        console.log("Time Threshold:", rebalanceAdapter.rebalanceTimeThreshold());
+        console.log("Max Deviation Long:", rebalanceAdapter.maxDeviationLong());
+        console.log("Max Deviation Short:", rebalanceAdapter.maxDeviationShort());
+        console.log("Is Inverted Assets:", rebalanceAdapter.isInvertedAssets());
+        console.log("Is Nova:", rebalanceAdapter.isNova());
+        console.log("Rebalance Operator:", rebalanceAdapter.rebalanceOperator());
+
+        console.log("\n=== SRebalanceAdapter State ===");
+        console.log("Oracle Price Last Rebalance:", rebalanceAdapter.oraclePriceAtLastRebalance());
+        console.log("Sqrt Price Last Rebalance:", rebalanceAdapter.sqrtPriceAtLastRebalance());
+        console.log("Time Last Rebalance:", rebalanceAdapter.timeAtLastRebalance());
+        console.log("Current Block Timestamp:", block.timestamp);
+
+        console.log("\n=== Oracle Data ===");
+        (uint256 currentPrice, uint160 currentSqrtPrice) = oracle.poolPrice();
+        console.log("Current Oracle Price:", currentPrice);
+        console.log("Current Sqrt Price:", currentSqrtPrice);
+
+        console.log("\n=== Rebalance Check ===");
+        (bool needed, uint256 priceThreshold, uint256 triggerTime) = rebalanceAdapter.isRebalanceNeeded(currentPrice);
+        console.log("Is Rebalance Needed:", needed);
+        console.log("Calculated Price Threshold:", priceThreshold);
+        
+        // Price Difference %
+        uint256 priceDiffPercent = priceThreshold > 1e18 ? ((priceThreshold - 1e18) * 100) / 1e18 : ((1e18 - priceThreshold) * 100) / 1e18;
+        uint256 diffScaled = priceThreshold > 1e18 ? priceThreshold - 1e18 : 1e18 - priceThreshold;
+        uint256 diffPercent = diffScaled * 100 / 1e18;
+        uint256 diffBps = diffScaled * 10000 / 1e18;
+        console.log("Price Difference (%):", diffPercent);
+        console.log("Price Difference (BPS):", diffBps);
+
+        console.log("Trigger Time:", triggerTime);
+        
+        // Time Difference
+        uint256 timeLast = rebalanceAdapter.timeAtLastRebalance();
+        if (block.timestamp >= timeLast) {
+            uint256 timeDiffSeconds = block.timestamp - timeLast;
+            uint256 timeDiffMinutes = timeDiffSeconds / 60;
+            console.log("Time Since Last Rebalance (minutes):", timeDiffMinutes);
+        } else {
+            console.log("Time Since Last Rebalance (minutes): negative?");
+        }
+
+        console.log("\n=== Lending Position ===");
+        (uint256 CL, uint256 CS, uint256 DL, uint256 DS) = lendingAdapter.getPosition();
+        console.log("Collateral Long:", CL);
+        console.log("Collateral Short:", CS);
+        console.log("Debt Long:", DL);
+        console.log("Debt Short:", DS);
     }
 
     // ** Helpers
